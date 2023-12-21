20fd30d41d258ab3684a3b236d776fba123435cd
DKA01 <152611883+DKA01@users.noreply.github.com>
2023-12-21 05:59:37 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/foot1.JPG";
+import image from "../images/foot1.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
